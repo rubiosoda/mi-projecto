2e2ffcaf917525bd4d637eb51507004f223946fd
rubiosoda <rubiosoriad@gmail.com>
2026-04-17 09:25:43 +0200

    Add files via upload

---
+++ judo montserrat/scripts.js
@@ -0,0 +1,13 @@
+
+function toggleMenu() {
+    const menu = document.getElementById("mainMenu");
+    menu.classList.toggle("mobile-open");
+    menu.classList.toggle("mobile-closed");
+}
+
+
+
+
+
+
+
